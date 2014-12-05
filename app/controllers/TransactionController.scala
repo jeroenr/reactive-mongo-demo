be0d1102140a894081c1c3f4c871b7bca3612724
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2014-12-05 10:00:02 +0100

    Moved Mongo stuff to service. Use Either for error handling

--- app/controllers/TransactionController.scala
+++ app/controllers/TransactionController.scala
@@ -1,14 +1,15 @@
 package controllers
 
 import play.api._
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.Json
 import play.api.mvc._
 import play.modules.reactivemongo.MongoController
-import play.modules.reactivemongo.json.collection.JSONCollection
+import services.dal.TransactionService
 import services.json.JsonActions._
 
 import services.json.MarshallableImplicits._
 
+// TODO: Would be great to get rid of this
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 /**
@@ -16,29 +17,20 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
  */
 
 object TransactionController extends Controller with MongoController {
-  def collection: JSONCollection = db.collection[JSONCollection]("transactions")
+  val transactionService = TransactionService(db)
 
   def create = JsonPostAction("mySchema") { jsonObject =>
-    collection.insert(jsonObject).map { lastError =>
-      Logger.debug(s"Successfully inserted with LastError: $lastError")
-      if(lastError.ok)
-        Created(Map("errors" -> Seq()).toJson)
-      else
-        InternalServerError(Map("errors" -> Seq(lastError.err.get)).toJson)
+    transactionService.insert(jsonObject).map { either =>
+      either match {
+        case Right(_) => Created(Map("errors" -> Seq()).toJson)
+        case Left(exc) => InternalServerError(wrapExceptionInJson(exc))
+      }
     }
   }
 
   def list = JsonGetAction { request =>
-    // let's do our query
-
-    val cursor = collection.find(Json.obj()).cursor[JsValue]
-
-    // gather all the JsObjects in a list
-    val futureTransactionsList = cursor.collect[List]()
-
-    // everything's ok! Let's reply with the array
-    futureTransactionsList.map { transactions =>
-      Ok(Map("transactions" -> transactions.map(_.toString().toMapOf[AnyRef]() - "_id")).toJson)
+    transactionService.find[Map[String,AnyRef]](Json.obj()).map { transactions =>
+      Ok(Map("transactions" -> (transactions.map(_ - "_id"))).toJson)
     }
   }
 }
---
+++ app/services/dal/TransactionService.scala
@@ -0,0 +1,33 @@
+package services.dal
+
+import play.api.Logger
+import play.api.libs.json.{JsValue, JsObject}
+import play.modules.reactivemongo.json.collection.JSONCollection
+import services.json.MarshallableImplicits._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+/**
+ * Created by jero on 05/12/14.
+ */
+class TransactionService(db: reactivemongo.api.DefaultDB, collectionName: String) {
+  def collection: JSONCollection = db.collection[JSONCollection](collectionName)
+
+  def insert(jsObject: JsObject) = collection.insert(jsObject).map { lastError =>
+    Logger.debug(s"Inserted with LastError: $lastError")
+    if(lastError.ok)
+      Right(jsObject)
+    else
+      Left(new RuntimeException(s"Failed to insert: ${lastError.errMsg.get}"))
+
+  }
+
+  def find[T](query: JsObject)(implicit m : Manifest[T]) = {
+    val cursor = collection.find(query).cursor[JsValue]
+    cursor.collect[List]().map(_.map(_.toString().fromJson[T]))
+  }
+}
+
+object TransactionService {
+  def apply(db: reactivemongo.api.DefaultDB): TransactionService = new TransactionService(db, "transactions")
+}
